fe9696ae4d5467c46b79970051ea0a8953c86acb
arabic0513 <rustamyusupov230@gmail.com>
2024-01-09 16:26:44 +0500

    contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -20,20 +20,18 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Contact Us</h6>
-                <h1 class="mb-5">Contact For Any Query</h1>
+                <h6 class="section-title bg-white text-center text-primary px-3">Контакт</h6>
+                <h1 class="mb-5">Контакт для любого запроса</h1>
             </div>
             <div class="row g-4">
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <h5>Get In Touch</h5>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos</p>
                     <div class="d-flex align-items-center mb-4">
                         <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
                             <i class="fa fa-map-marker-alt text-white"></i>
                         </div>
                         <div class="ms-3">
-                            <h5 class="text-primary">Office</h5>
-                            <p class="mb-0">123 Street, New York, USA</p>
+                            <h5 class="text-primary">Офис</h5>
+                            <p class="mb-0">Sodiq Azimov ko'chasi 68, 100100, Тоshkent, Toshkent</p>
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-4">
@@ -41,23 +39,23 @@
                             <i class="fa fa-phone-alt text-white"></i>
                         </div>
                         <div class="ms-3">
-                            <h5 class="text-primary">Mobile</h5>
+                            <h5 class="text-primary">Мобильный</h5>
                             <p class="mb-0">+012 345 67890</p>
                         </div>
                     </div>
-                    <div class="d-flex align-items-center">
-                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
-                            <i class="fa fa-envelope-open text-white"></i>
-                        </div>
-                        <div class="ms-3">
-                            <h5 class="text-primary">Email</h5>
-                            <p class="mb-0">info@example.com</p>
-                        </div>
-                    </div>
+{{--                    <div class="d-flex align-items-center">--}}
+{{--                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">--}}
+{{--                            <i class="fa fa-envelope-open text-white"></i>--}}
+{{--                        </div>--}}
+{{--                        <div class="ms-3">--}}
+{{--                            <h5 class="text-primary">Email</h5>--}}
+{{--                            <p class="mb-0">info@example.com</p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <iframe class="position-relative rounded w-100 h-100"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                            src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5994.466696009942!2d69.28883!3d41.303787!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDE4JzEzLjYiTiA2OcKwMTcnMTkuOCJF!5e0!3m2!1suz!2s!4v1704798997799!5m2!1suz!2s"
                             frameborder="0" style="min-height: 300px; border:0;" allowfullscreen="" aria-hidden="false"
                             tabindex="0"></iframe>
                 </div>
@@ -66,30 +64,30 @@
                         <div class="row g-3">
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="name" placeholder="Your Name">
-                                    <label for="name">Your Name</label>
+                                    <input type="text" class="form-control" id="name" placeholder="Ваше имя">
+                                    <label for="name">Ваше имя</label>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <input type="email" class="form-control" id="email" placeholder="Your Email">
-                                    <label for="email">Your Email</label>
+                                    <input type="email" class="form-control" id="email" placeholder="Электронной почты">
+                                    <label for="email">Электронной почты</label>
                                 </div>
                             </div>
                             <div class="col-12">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                    <label for="subject">Subject</label>
+                                    <input type="text" class="form-control" id="phone" placeholder="Телефон">
+                                    <label for="subject">Телефон</label>
                                 </div>
                             </div>
                             <div class="col-12">
                                 <div class="form-floating">
-                                    <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 100px"></textarea>
-                                    <label for="message">Message</label>
+                                    <textarea class="form-control" placeholder="Сообщение" id="message" style="height: 100px"></textarea>
+                                    <label for="message">Сообщение</label>
                                 </div>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                                <button class="btn btn-primary w-100 py-3" type="submit">Отправить сообщение</button>
                             </div>
                         </div>
                     </form>
