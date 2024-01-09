9e459a31c3eeff7e8db15df4f774eeb52a82c232
User <elorgray@gmail.com>
2024-01-09 21:40:16 +0500

    packages.blade.php changes

--- resources/views/pages/packages.blade.php
+++ resources/views/pages/packages.blade.php
@@ -24,23 +24,23 @@
             </div>
             <div class="row g-4 justify-content-center">
                 @foreach($country as $co)
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="package-item">
-                        <div class="overflow-hidden">
-                            <img class="img-fluid" src="img/package-1.jpg" alt="">
-                        </div>
-                        <div class="d-flex border-bottom">
-                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-map-marker-alt text-primary me-2"></i>{{$co->name}}</small>
-                        </div>
-                        <div class="text-center p-4">
-                            <h3 class="mb-0">{{$co->price}}</h3>
-                            <p></p>
-                            <div class="d-flex justify-content-center mb-2">
-                                <a href="#" class="btn btn-sm btn-primary px-3 border-end">Читать далее</a>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="package-item">
+                            <div class="overflow-hidden">
+                                <img class="img-fluid" src="{{asset('storage/' . $co->img)}}" alt="">
+                            </div>
+                            <div class="d-flex border-bottom">
+                                <h3 class="flex-fill text-center border-end py-2"><i class="fa fa-map-marker-alt text-primary me-2"></i>{{$co->name}}</h3>
+                            </div>
+                            <div class="text-center p-4">
+                                <h3 class="mb-0">{{$co->price}}</h3>
+                                <p>{{$co->description}}</p>
+                                <div class="d-flex justify-content-center mb-2">
+                                    <a href="{{route('country.show',$co->id)}}" class="btn btn-sm btn-primary px-3 border-end">Читать далее</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                     @endforeach
         </div>
     </div>
