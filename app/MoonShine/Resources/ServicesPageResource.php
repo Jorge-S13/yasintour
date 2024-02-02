9affa088f3633a839c5a3b7becfb48b0411e6eae
arabic0513 <rustamyusupov230@gmail.com>
2024-02-02 17:51:17 +0500

    Service Page to spatie

--- app/MoonShine/Resources/ServicesPageResource.php
+++ app/MoonShine/Resources/ServicesPageResource.php
@@ -11,6 +11,7 @@ use MoonShine\Fields\Text;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
 use MoonShine\Fields\ID;
+use VI\MoonShineSpatieTranslatable\Fields\Translatable;
 
 class ServicesPageResource extends ModelResource
 {
@@ -23,25 +24,25 @@ class ServicesPageResource extends ModelResource
         return [
             Block::make([
                 ID::make()->sortable(),
-                Text::make('Navbar Text', 'navbar_text'),
-                Text::make('Services h6', 'services_h6'),
-                Text::make('Services h1', 'services_h1'),
-                Text::make('Item-1 h5', 'item1_h5'),
-                Text::make('Item-1 p', 'item1_p'),
-                Text::make('Item-2 h5', 'item2_h5'),
-                Text::make('Item-2 p', 'item2_p'),
-                Text::make('Item-3 h5', 'item3_h5'),
-                Text::make('Item-3 p', 'item3_p'),
-                Text::make('Item-4 h5', 'item4_h5'),
-                Text::make('Item-4 p', 'item4_p'),
-                Text::make('Item-5 h5', 'item5_h5'),
-                Text::make('Item-5 p', 'item5_p'),
-                Text::make('Item-6 h5', 'item6_h5'),
-                Text::make('Item-6 p', 'item6_p'),
-                Text::make('Item-7 h5', 'item7_h5'),
-                Text::make('Item-7 p', 'item7_p'),
-                Text::make('Item-8 h5', 'item8_h5'),
-                Text::make('Item-8 p', 'item8_p'),
+                Translatable::make('Navbar Text', 'navbar_text')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Services h6', 'services_h6')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Services h1', 'services_h1')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-1 h5', 'item1_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-1 p', 'item1_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-2 h5', 'item2_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-2 p', 'item2_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-3 h5', 'item3_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-3 p', 'item3_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-4 h5', 'item4_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-4 p', 'item4_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-5 h5', 'item5_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-5 p', 'item5_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-6 h5', 'item6_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-6 p', 'item6_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-7 h5', 'item7_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-7 p', 'item7_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-8 h5', 'item8_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Item-8 p', 'item8_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
             ]),
         ];
     }
