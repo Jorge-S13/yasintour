60562bf3b71d43590038d211ced0eac7890b4a49
arabic0513 <rustamyusupov230@gmail.com>
2024-02-02 17:47:42 +0500

    Main Page to spatie

--- app/MoonShine/Resources/MainPageResource.php
+++ app/MoonShine/Resources/MainPageResource.php
@@ -11,6 +11,7 @@ use MoonShine\Fields\Text;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
 use MoonShine\Fields\ID;
+use VI\MoonShineSpatieTranslatable\Fields\Translatable;
 
 class MainPageResource extends ModelResource
 {
@@ -23,17 +24,17 @@ class MainPageResource extends ModelResource
         return [
             Block::make([
                 ID::make()->sortable(),
-                Text::make('Navbar Text', 'navbar_text'),
-                Text::make('Process h6','process_h6'),
-                Text::make('Process h1','process_h1'),
-                Text::make('Process item1 h5','process_item1_h5'),
-                Text::make('Process item1 p','process_item1_p'),
-                Text::make('Process item2 h5','process_item2_h5'),
-                Text::make('Process item2 p','process_item2_p'),
-                Text::make('Process item3 h5','process_item3_h5'),
-                Text::make('Process item3 p','process_item3_p'),
-                Text::make('Feedback h6','feedback_h6'),
-                Text::make('Feedback h1','feedback_h1'),
+                Translatable::make('Navbar Text', 'navbar_text')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process h6','process_h6')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process h1','process_h1')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process item1 h5','process_item1_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process item1 p','process_item1_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process item2 h5','process_item2_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process item2 p','process_item2_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process item3 h5','process_item3_h5')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Process item3 p','process_item3_p')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Feedback h6','feedback_h6')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
+                Translatable::make('Feedback h1','feedback_h1')->requiredLanguages([config('app.fallback_locale'), 'uz', 'ru', 'en']),
 
             ]),
         ];
